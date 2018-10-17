ed297012b71b82c89cb595f8e384c5bd20ad0a82
Roman Kniazev <kameronton@gmail.com>
2018-10-17 14:15:09 +0200

    algo refined, tests added

--- algo.py
+++ algo.py
@@ -1,6 +1,7 @@
-from math import *
+from math import log
 import networkx as nx
 from random import random
+from copy import deepcopy
 
 def preprocessing(G):
     for i in range(G.number_of_nodes()):
@@ -12,8 +13,10 @@ def MIS(G):
     '''G = nx.Graph()'''
     '''je suppose que les noeuds sont dans [|0, n-1|]'''
     
-    preprocessing(G)
-    
+    G.add_nodes_from(G.nodes, active = True, MIS = False)
+    history = []
+    history.append(deepcopy(G))
+
     n = D = G.number_of_nodes()
     
     M = 34#faudrait trouver la vraie valeur
@@ -21,49 +24,73 @@ def MIS(G):
     for i in range(int(log(D))+1):
         for j in range(int(M*log(n))+1):
             
+            G.add_nodes_from(G.nodes, v = 0, received = False)
             
-            v = [0 for _ in range(n)]
-            received = [False for _ in range(n)]
             
             #Exchange 1 : emission            
-            for u in range(n):
+            for u in G.nodes:
                 if G.nodes[u]["active"]:
                     p = random()
                     if p <= 1./ (2**(log(D)-i)):
                         for voisin in G.adj[u]:
-                            received[voisin] = True
-                        v[u] = 1
+                            G.nodes[voisin]["received"] = True
+                        G.nodes[u]["v"] = 1
                 
             #reception
-            for u in range(n):
+            for u in G.nodes:
                 if G.nodes[u]["active"]:
-                    if received[u]:
-                        v[u] = 0
+                    if G.nodes[u]["received"]:
+                        G.nodes[u]["v"] = 0
             
-            received = [False for _ in range(n)]
+            G.add_nodes_from(G.nodes, received = False)
             
             #Exchange 2 : emission
-            for u in range(n):
+            for u in G.nodes:
                 if G.nodes[u]["active"]:
-                    if v[u] == 1:
+                    if G.nodes[u]['v'] == 1:
                         for voisin in G.adj[u]:
-                            received[voisin] = True
+                            G.nodes[voisin]['received'] = True
                         G.nodes[u]["active"] = False
                         G.nodes[u]["MIS"] = True 
                     
             #reception
-            for u in range(n):
+            for u in G.nodes:
                 if G.nodes[u]["active"]:
-                    if received[u]:
+                    if G.nodes[u]["received"]:
                         G.nodes[u]["active"] = False
+            
+            history.append(deepcopy(G))
 
-    return [G.nodes[i]["MIS"] for i in range(n)]
+    return (G, history)
+
+def colors(G):
+    colors = []
+    for node in G.nodes:
+        if G.nodes[node]["MIS"] == True:
+            colors.append("green")
+
+        elif G.nodes[node]["active"] == True:
+            colors.append("red")
+    
+        else:
+            colors.append("black")
+    return colors
+
 
 
 def test_1():
     G = nx.Graph()
     G.add_nodes_from([0, 1, 2])
     G.add_edges_from([(0, 1), (0, 2), (1, 2)])
-    L = MIS(G)
-    assert(L == [True, False, True] or L == [False, True, False])
+    L = MIS(G)[0]
+    mis = [L.nodes[node]["MIS"] for node in L.nodes]
+    assert(mis == [True, False, False] or mis == [False, True, False] or mis == [False, False, True])
+
+
+    G = nx.Graph()
+    G.add_nodes_from([0, 1, 2])
+    G.add_edges_from([(0, 1), (0, 2)])
+    L = MIS(G)[0]
+    mis = [L.nodes[node]["MIS"] for node in L.nodes]
+    assert(mis == [True, False, False] or mis == [False, True, True])
     
